4f92032820f7c20455007ac60e24d52c4846e230
Seo Hansol <ciwlx@kaist.ac.kr>
2018-12-12 15:07:56 +0900

    Add CallStack

--- src/HistoryTable.cpp
+++ src/HistoryTable.cpp
@@ -6,22 +6,25 @@ void HistoryTable::addEntry(int line, int value) {
   HistoryTableEntry entry = HistoryTableEntry();
   entry.lineNumber = line;
   entry.value.integer = value;
+  this->table.push_back(entry);
 }
 
 void HistoryTable::addEntry(int line, float value) {
   HistoryTableEntry entry = HistoryTableEntry();
   entry.lineNumber = line;
   entry.value.floating = value;
+  this->table.push_back(entry);
 }
 
 void HistoryTable::addEntry(int line, void* value) {
   HistoryTableEntry entry = HistoryTableEntry();
   entry.lineNumber = line;
   entry.value.pointer = value;
+  this->table.push_back(entry);
 }
 
 void HistoryTable::printHistory(std::string name) {
   for (std::vector<HistoryTableEntry>::iterator it = this->table.begin(); it != this->table.end(); it++) {
-    std::cout << name << " = " << (*it).value << " at line " << (*it).lineNumber << std::endl;
+    //std::cout << name << " = " << (*it).value << " at line " << (*it).lineNumber << std::endl;
   }
 }
